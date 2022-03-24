24e4c54fbef65cfbd44adb034cf7837ebf3a7e59
Fasasoft <fasilteshsoft@gmail.com>
2022-03-24 11:33:01 -0700

    swap the values of two integer

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,9 +2,9 @@
 
 /**
  * swap_int - swap variable values of two integr a andd b
- * @a: pointer 1
- * @b: pointer 2
- * Return: void
+ * @a: pointer to an int that will be updated
+ * @b: pointer to an int that will be updated 
+ * Return: void that means our answer is correct
 */
 
 void swap_int (int *a, int *b)	
